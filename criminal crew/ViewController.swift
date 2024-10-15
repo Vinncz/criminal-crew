8c221ae2ade78b3f386018627680eefeb8c5bc14
Vinn <kevgun03@gmail.com>
2024-10-15 17:05:08 +0700

    Initial Commit

---
+++ criminal crew/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  criminal crew
+//
+//  Created by Vin on 15/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
